4d860941cbd674b4dd58459cfdc8fac8cde2a543
LENOVO <kakaderahul70@gmail.com>
2022-08-26 18:08:31 +0530

    first commit

---
+++ src/Packu/Test1.java
@@ -0,0 +1,18 @@
+package Packu;
+
+public class Test1 {
+	
+	public static void main(String[] args)
+	{
+		
+		String str="hungry";
+		if (str=="hungry")
+			System.out.println("need to eat something");
+		else
+			System.out.println("No need to eat");
+		
+		
+	}
+	
+
+}
